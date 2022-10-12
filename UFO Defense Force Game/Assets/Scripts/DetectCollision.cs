f1cf39734e6edb03a905c26279b698cb644a15a2
David Reyes <10908285@uvu.edu>
2022-10-12 16:45:25 -0600

    Particles code and background music and sound effects

--- UFO Defense Force Game/Assets/Scripts/DetectCollision.cs
+++ UFO Defense Force Game/Assets/Scripts/DetectCollision.cs
@@ -6,9 +6,9 @@ public class DetectCollision : MonoBehaviour
 {
     public ScoreManager scoreManager; //A variable to hold the reference to score manager
     public int scoreToGive; 
-    public ParticleSysyem explosionParticle;
+    public ParticleSystem explosionParticle; //Store the particle system
 
-
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -18,12 +18,16 @@ public class DetectCollision : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        explosionParticle.Play(); //plays particle effect. 
+        Explosion();
         scoreManager.IncreaseScore(scoreToGive); //Increase score 
         Destroy(other.gameObject); // Destroy this game object (whatever the script is on)
         Destroy(gameObject); // Destroy other game object that passes trhough the trigger
        
-        
+    }
+
+    public void Explosion()
+    {
+        Instantiate(explosionParticle, transform.position, transform.rotation);
     }
    
 }
