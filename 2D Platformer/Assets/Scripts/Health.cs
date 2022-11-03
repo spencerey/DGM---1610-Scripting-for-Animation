da16d34ac73a2d7fa92690dc2296544aa512b68c
David Reyes <10908285@uvu.edu>
2022-11-02 18:04:27 -0600

    Player Health code

---
+++ 2D Platformer/Assets/Scripts/Health.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int maxHealth = 3;
+    public int currentHealth;
+    public float deathDelay ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+  
+
+    public void TakeDamage (int dmgAmount)
+    {
+        currentHealth -= dmgAmount;
+
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject, deathDelay);
+        }
+    }
+
+    public void AddHealth (int healAmount)
+    {
+        currentHealth += healAmount;
+
+        if(currentHealth >= maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+}
